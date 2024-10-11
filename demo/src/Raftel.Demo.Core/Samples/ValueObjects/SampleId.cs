7f10174c3d94d3681bc638c3c131778485ad1e03
Francisco Sierra Luciarte <62906570+franciscofsl@users.noreply.github.com>
2024-10-11 18:57:23 +0200

    Add core sample entity

---
+++ demo/src/Raftel.Demo.Core/Samples/ValueObjects/SampleId.cs
@@ -0,0 +1,13 @@
+﻿using Raftel.Core.BaseTypes;
+
+namespace Raftel.Demo.Core.Samples.ValueObjects;
+
+public sealed record SampleId : EntityId
+{
+    public static explicit operator SampleId(Guid id) => new()
+    {
+        Value = id
+    };
+
+    public static implicit operator Guid(SampleId id) => id.Value;
+}
