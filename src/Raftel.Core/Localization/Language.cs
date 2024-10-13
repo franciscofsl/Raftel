f632318c1d95faf04675647655d3d129146c84a6
Francisco Sierra Luciarte <62906570+franciscofsl@users.noreply.github.com>
2024-10-13 08:12:17 +0200

    feat(localization): add language models

---
+++ src/Raftel.Core/Localization/Language.cs
@@ -0,0 +1,19 @@
+﻿using Raftel.Core.BaseTypes;
+using Raftel.Core.Localization.ValueObjects;
+
+namespace Raftel.Core.Localization;
+
+public sealed class Language : AggregateRoot<LanguageId>
+{
+    private Language()
+    {
+    }
+
+    public static Language Create()
+    {
+        return new Language
+        {
+            Id = EntityIdGenerator.Create<LanguageId>()
+        };
+    }
+}
