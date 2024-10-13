13a00d30d9cb01bbe52cf9160fc196cd943b179c
Francisco Sierra Luciarte <62906570+franciscofsl@users.noreply.github.com>
2024-10-13 08:14:52 +0200

    feat(localization): add translation resource

--- src/Raftel.Core/Localization/Language.cs
+++ src/Raftel.Core/Localization/Language.cs
@@ -5,15 +5,30 @@ namespace Raftel.Core.Localization;
 
 public sealed class Language : AggregateRoot<LanguageId>
 {
+    public string Name { get; private set; }
+    public string IsoCode { get; private set; }
+
+    private readonly List<TranslationResource> _resources = new();
+    public IReadOnlyCollection<TranslationResource> Resources => _resources.AsReadOnly();
+
     private Language()
     {
     }
 
-    public static Language Create()
+    public static Language Create(string name, string isoCode)
     {
         return new Language
         {
-            Id = EntityIdGenerator.Create<LanguageId>()
+            Id = EntityIdGenerator.Create<LanguageId>(),
+            Name = name,
+            IsoCode = isoCode
         };
+    }
+
+    public TranslationResource AddTranslationResource(string key, string value)
+    {
+        var resource = TranslationResource.Create(key, value);
+        _resources.Add(resource);
+        return resource;
     }
 }
