b21739bfad1d8b9cd9825c6899bc559574608b06
Francisco Sierra Luciarte <62906570+franciscofsl@users.noreply.github.com>
2024-10-13 09:03:38 +0200

    feat(localization): add create translation resource command

---
+++ src/Raftel.Application/Localization/Commands/AddTranslationResource/AddTranslationResourceCommand.cs
@@ -0,0 +1,7 @@
+﻿using Raftel.Application.Cqrs.Commands;
+using Raftel.Core.Localization.ValueObjects;
+using Raftel.Shared.Results;
+
+namespace Raftel.Application.Localization.Commands.AddTranslationResource;
+
+public record AddTranslationResourceCommand(LanguageId LanguageId, string Key, string Value) : ICommand<Result>;
---
+++ src/Raftel.Application/Localization/Commands/AddTranslationResource/AddTranslationResourceCommandHandler.cs
@@ -0,0 +1,16 @@
+﻿using Raftel.Application.Cqrs.Commands;
+using Raftel.Core.Localization;
+using Raftel.Shared.Results;
+
+namespace Raftel.Application.Localization.Commands.AddTranslationResource;
+
+public class AddTranslationResourceCommandHandler(ILanguagesRepository languagesRepository)
+    : ICommandHandler<AddTranslationResourceCommand, Result>
+{
+    public async Task<Result> Handle(AddTranslationResourceCommand command, CancellationToken token = default)
+    {
+        var language = await languagesRepository.GetAsync(command.LanguageId);
+
+        return language.AddTranslationResource(command.Key, command.Value);
+    }
+}
