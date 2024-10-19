1519bbd3be5f3ce1d6683505f306c58f5b70c2ba
Francisco Sierra Luciarte <62906570+franciscofsl@users.noreply.github.com>
2024-10-19 16:47:21 +0200

    feat(storage): add folder entity

---
+++ src/Raftel.Core/Storage/Document.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Raftel.Core.BaseTypes;
+using Raftel.Shared.GuidGenerators;
+
+namespace Raftel.Core.Storage;
+
+public sealed class Document : AggregateRoot<Guid>
+{
+    private Document()
+    {
+    }
+
+    public static Document Create(string name, string extension, long size, Guid blobFileId)
+    {
+        return new Document
+        {
+            Id = SequentialGuidGenerator.Create(),
+            Name = name,
+            Extension = extension,
+            Size = size,
+            CreationDate = DateTime.UtcNow,
+            BlobFileId = blobFileId,
+        };
+    }
+
+    public string Name { get; private set; }
+    public string Extension { get; private set; }
+    public long Size { get; private set; }
+    public DateTime CreationDate { get; private set; }
+    public Guid BlobFileId { get; private set; }
+}
---
+++ tests/Raftel.Core.Tests/Storage/FolderTest.cs
@@ -0,0 +1,89 @@
+﻿using Raftel.Core.Storage;
+
+namespace Raftel.Core.Tests.Storage;
+
+public class FolderTests
+{
+    [Fact]
+    public void CreateFolder_ShouldCreateSubFolder_WhenNameIsUnique()
+    {
+        var parentFolder = Folder.Create("Parent");
+        var result = parentFolder.CreateFolder("SubFolder");
+
+        result.Success.Should().BeTrue();
+        result.Value.Should().NotBeNull();
+        result.Value.Name.Should().Be("SubFolder");
+        parentFolder.SubFolders.Should().ContainSingle().Which.Should().BeEquivalentTo(result.Value);
+    }
+
+    [Fact]
+    public void CreateFolder_ShouldFail_WhenNameIsDuplicated()
+    {
+        var parentFolder = Folder.Create("Parent");
+        parentFolder.CreateFolder("SubFolder");
+
+        var result = parentFolder.CreateFolder("SubFolder");
+
+        result.Success.Should().BeFalse();
+        result.Error.Code.Should().Be(StorageErrors.DuplicatedFolder);
+        parentFolder.SubFolders.Should().HaveCount(1);
+    }
+
+    [Fact]
+    public void RemoveSubFolder_ShouldRemoveFolder_WhenFolderExists()
+    {
+        var parentFolder = Folder.Create("Parent");
+        var subFolder = parentFolder.CreateFolder("SubFolder").Value;
+
+        parentFolder.RemoveSubFolder(subFolder);
+
+        parentFolder.SubFolders.Should().BeEmpty();
+    }
+
+    [Fact]
+    public void RemoveSubFolder_ShouldNotRemoveFolder_WhenFolderDoesNotExist()
+    {
+        var parentFolder = Folder.Create("Parent");
+        var nonExistentFolder = Folder.Create("NonExistentFolder");
+
+        parentFolder.RemoveSubFolder(nonExistentFolder);
+
+        parentFolder.SubFolders.Should().BeEmpty();
+    }
+
+    [Fact]
+    public void AddDocument_ShouldAddDocument_WhenValidParametersAreProvided()
+    {
+        var folder = Folder.Create("Documents");
+        var blobFileId = Guid.NewGuid();
+        var result = folder.AddDocument("Document1", ".txt", 1024, blobFileId);
+
+        result.Success.Should().BeTrue();
+        result.Value.Should().NotBeNull();
+        result.Value.Name.Should().Be("Document1");
+        folder.Documents.Should().ContainSingle().Which.Should().BeEquivalentTo(result.Value);
+    }
+
+    [Fact]
+    public void RemoveDocument_ShouldRemoveDocument_WhenDocumentExists()
+    {
+        var folder = Folder.Create("Documents");
+        var blobFileId = Guid.NewGuid();
+        var document = folder.AddDocument("Document1", ".txt", 1024, blobFileId).Value;
+
+        folder.RemoveDocument(document);
+
+        folder.Documents.Should().BeEmpty();
+    }
+
+    [Fact]
+    public void RemoveDocument_ShouldNotRemoveDocument_WhenDocumentDoesNotExist()
+    {
+        var folder = Folder.Create("Documents");
+        var nonExistentDocument = Document.Create("NonExistentDocument", ".txt", 0, Guid.NewGuid());
+
+        folder.RemoveDocument(nonExistentDocument);
+
+        folder.Documents.Should().BeEmpty();
+    }
+}
