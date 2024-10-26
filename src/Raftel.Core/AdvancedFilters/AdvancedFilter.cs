5db0497ec2545a2955ee8cf93abb560d0be23638
Francisco Sierra Luciarte <62906570+franciscofsl@users.noreply.github.com>
2024-10-26 20:29:41 +0200

    feat(query builder) add fluent style types

--- src/Raftel.Core/AdvancedFilters/AdvancedFilter.cs
+++ src/Raftel.Core/AdvancedFilters/AdvancedFilter.cs
@@ -2,6 +2,27 @@
 
 public class AdvancedFilter
 {
-    public Condition Condition { get; set; }
-    public List<Rule> Rules { get; set; } = new();
+    private AdvancedFilter()
+    {
+    }
+
+    public static AdvancedFilter<TModel> ForModel<TModel>(Condition condition = Condition.And)
+    {
+        return new AdvancedFilter<TModel>(condition);
+    }
+}
+
+public class AdvancedFilter<TModel>(Condition Condition = Condition.And)
+{
+    private List<Rule> _rules = new();
+
+    public AdvancedFilter<TModel> StartsWith(Func<TModel, object> func, string value)
+    {
+        return this;
+    }
+
+    public bool Satisfy(TModel model)
+    {
+        return true;
+    }
 }
---
+++ tests/Raftel.Core.Tests/AdvancedFilters/AdvancedFilterTest.cs
@@ -0,0 +1,26 @@
+﻿using Raftel.Core.AdvancedFilters;
+
+namespace Raftel.Core.Tests.AdvancedFilters;
+
+public class AdvancedFilterTest
+{
+    [Fact]
+    public void AdvancedFilter_ShouldCreateValidExpression_For_Text_Equals()
+    {
+        var advancedFilter = AdvancedFilter
+            .ForModel<Pirate>()
+            .StartsWith(_ => _.Name, "Luffy");
+
+        var pirate = new Pirate()
+        {
+            Name = "Luffy"
+        };
+
+        advancedFilter.Satisfy(pirate).Should().BeTrue();
+    }
+
+    private class Pirate
+    {
+        public string Name { get; set; }
+    }
+}
