8c93741c1e3fc2545db742b87cf9f5a5c4b6bcd0
Francisco Sierra Luciarte <62906570+franciscofsl@users.noreply.github.com>
2024-10-26 18:53:15 +0200

    feat(file explorer): query to retrieve folders

---
+++ src/Raftel.Application/BlobStorage/Queries/GetFolders/GetFoldersQuery.cs
@@ -0,0 +1,6 @@
+﻿using Raftel.Application.Contracts.Storage;
+using Raftel.Application.Cqrs.Queries;
+
+namespace Raftel.Application.BlobStorage.Queries.GetFolders;
+
+public record GetFoldersQuery(Guid? ParentFolderId) : IQuery<List<FolderDto>>;
