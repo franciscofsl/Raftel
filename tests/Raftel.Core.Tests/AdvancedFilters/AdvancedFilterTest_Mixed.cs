666e3bf4139874b7d0facffd134b3b4574d26ce1
Francisco Sierra Luciarte <62906570+franciscofsl@users.noreply.github.com>
2024-11-02 07:33:15 +0100

    test(query builder): add filter case for string equality and bounty greater than or equal

---
+++ tests/Raftel.Core.Tests/AdvancedFilters/AdvancedFilterTest_Mixed.cs
@@ -0,0 +1,21 @@
+﻿using Raftel.Core.AdvancedFilters;
+
+namespace Raftel.Core.Tests.AdvancedFilters;
+
+public partial class AdvancedFilterTest
+{
+    [Fact]
+    public void
+        AdvancedFilter_ShouldFilterForStringEqualAndBountyGreaterThanOrEqual_IfNameEqualAndBountyGreaterThanOrEqual()
+    {
+        var filter = AdvancedFilterBuilder
+            .ForModel<Pirate>()
+            .And(b => b.Equal(_ => _.Name, "Luffy"))
+            .And(b => b.GreaterThanOrEqual(_ => _.Bounty, 66000000))
+            .Build();
+
+        filter(Pirates.Mugiwaras.Luffy).Should().BeTrue();
+        filter(Pirates.Mugiwaras.Nami).Should().BeFalse();
+    }
+
+}
