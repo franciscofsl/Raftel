fe061c1b02f176475f89142557977c26197588e1
Francisco Sierra Luciarte <62906570+franciscofsl@users.noreply.github.com>
2025-04-10 20:31:39 +0200

    add queries

---
+++ src/Raftel.Application/Queries/IQueryDispatcher.cs
@@ -0,0 +1,8 @@
+﻿using Raftel.Domain.Abstractions;
+
+namespace Raftel.Application.Queries;
+
+public interface IQueryDispatcher
+{
+    Task<Result<TResponse>> DispatchAsync<TQuery, TResponse>(TQuery query) where TQuery : IQuery<TResponse>;
+}
