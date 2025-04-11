2b7739d04b9c51006398724a4688333bd36b0757
Francisco Sierra Luciarte <62906570+franciscofsl@users.noreply.github.com>
2025-04-11 15:59:46 +0200

    add documentation

---
+++ tools/Raftel.Cli/Program.cs
@@ -0,0 +1,12 @@
+﻿using Spectre.Console.Cli;
+
+var app = new CommandApp();
+
+app.Configure(config =>
+{
+    config.SetApplicationName("raftel-tools");
+    config.AddCommand<GenerateTocCommand>("docfx generate-toc")
+        .WithDescription("Genera un toc.yml agrupado por ensamblado desde los archivos .yml generados por DocFX");
+});
+
+return app.Run(args);
