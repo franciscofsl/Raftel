0460a662690f8a2415c666b3256ae22862a9bcc0
Francisco Sierra Luciarte <62906570+franciscofsl@users.noreply.github.com>
2025-04-14 19:38:20 +0200

    Refactor OrSpecification constructor to use primary constructor syntax

--- src/Raftel.Domain/Specifications/OrSpecification.cs
+++ src/Raftel.Domain/Specifications/OrSpecification.cs
@@ -6,30 +6,16 @@ namespace Raftel.Domain.Specifications;
 /// Represents a logical OR specification that combines two other specifications.
 /// </summary>
 /// <typeparam name="TModel">The type of the model to which the specification is applied.</typeparam>
-public class OrSpecification<TModel> : Specification<TModel>
+public class OrSpecification<TModel>(Specification<TModel> left, Specification<TModel> right) : Specification<TModel>
 {
-    private readonly Specification<TModel> _left;
-    private readonly Specification<TModel> _right;
-
-    /// <summary>
-    /// Initializes a new instance of the <see cref="OrSpecification{TModel}"/> class.
-    /// </summary>
-    /// <param name="left">The left specification to combine.</param>
-    /// <param name="right">The right specification to combine.</param>
-    public OrSpecification(Specification<TModel> left, Specification<TModel> right)
-    {
-        _left = left;
-        _right = right;
-    }
-
     /// <summary>
     /// Converts the specification to an expression.
     /// </summary>
     /// <returns>A <see cref="Expression{Func{TModel, bool}}"/> that represents the logical OR between the two specifications.</returns>
     public override Expression<Func<TModel, bool>> ToExpression()
     {
-        var leftExpr = _left.ToExpression();
-        var rightExpr = _right.ToExpression();
+        var leftExpr = left.ToExpression();
+        var rightExpr = right.ToExpression();
         var parameter = Expression.Parameter(typeof(TModel));
 
         var body = Expression.OrElse(
