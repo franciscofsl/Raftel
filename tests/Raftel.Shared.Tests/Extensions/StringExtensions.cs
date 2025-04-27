3b578d23b2e1008be4ba7ab178a60d90690aa4f4
Francisco Sierra Luciarte <62906570+franciscofsl@users.noreply.github.com>
2025-04-27 23:31:03 +0200

    feat: add unit tests for StringExtensions.ToCamelCase method

---
+++ tests/Raftel.Shared.Tests/Extensions/StringExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Raftel.Shared.Extensions;
+using Shouldly;
+
+namespace Raftel.Shared.Tests.Extensions;
+
+public class StringExtensionsTests
+{
+    [Theory]
+    [InlineData(null, null)]
+    [InlineData("", "")]
+    [InlineData("camelCase", "camelCase")]
+    [InlineData("CamelCaseCase", "camelCaseCase")]
+    [InlineData("A", "a")]
+    [InlineData("a", "a")]
+    public void ToCamelCase_ShouldReturnExpectedResult(string input, string expected)
+    {
+        var result = input.ToCamelCase();
+        result.ShouldBe(expected);
+    }
+}
