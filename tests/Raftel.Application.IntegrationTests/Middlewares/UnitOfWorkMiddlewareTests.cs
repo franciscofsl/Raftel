59edd5921408902c4b4964f8a4f357fafc9d1f98
Francisco Sierra Luciarte <62906570+franciscofsl@users.noreply.github.com>
2025-04-30 19:13:01 +0200

    refactor(middleware): rename middleware files and update project references

---
+++ tests/Raftel.Application.IntegrationTests/Middlewares/UnitOfWorkMiddlewareTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Raftel.Application.Commands;
+using Raftel.Tests.Common.Application.Pirates.CreatePirate;
+using Raftel.Tests.Common.Domain;
+using Raftel.Tests.Common.Infrastructure;
+using Shouldly;
+
+namespace Raftel.Application.IntegrationTests.Middlewares;
+
+public class UnitOfWorkMiddlewareTests : IntegrationTestBase
+{
+    [Fact]
+    public async Task CommitAsync_ShouldPersistData_UsingMiddleware()
+    {
+        await ExecuteScopedAsync(async sp =>
+        {
+            var commandDispatcher = sp.GetService<ICommandDispatcher>();
+
+            var command = new CreatePirateCommand("Ace", 9514361);
+            var result = await commandDispatcher!.DispatchAsync(command);
+            result.IsSuccess.ShouldBeTrue();
+
+            var repository = sp.GetRequiredService<IPirateRepository>();
+
+            var loaded = await repository.ListAllAsync();
+            loaded.ShouldHaveSingleItem();
+            loaded.ShouldContain(_ => _.Name == command.Name && _.Bounty == command.Bounty);
+        });
+    }
+}
--- tests/Raftel.Infrastructure.Tests/Data/UnitOfWorkTests.cs
+++ tests/Raftel.Infrastructure.Tests/Data/UnitOfWorkTests.cs
@@ -56,23 +56,4 @@ public class UnitOfWorkTests : IntegrationTestBase
             pirate.ShouldBe(loaded);
         });
     }
-
-    [Fact]
-    public async Task CommitAsync_ShouldPersistData_UsingMiddleware()
-    {
-        await ExecuteScopedAsync(async sp =>
-        {
-            var commandDispatcher = sp.GetService<ICommandDispatcher>();
-
-            var command = new CreatePirateCommand("Ace", 9514361);
-            var result = await commandDispatcher.DispatchAsync(command);
-            result.IsSuccess.ShouldBeTrue();
-
-            var repository = sp.GetRequiredService<IPirateRepository>();
-
-            var loaded = await repository.ListAllAsync();
-            loaded.ShouldHaveSingleItem();
-            loaded.ShouldContain(_ => _.Name == command.Name && _.Bounty == command.Bounty);
-        });
-    }
 }
