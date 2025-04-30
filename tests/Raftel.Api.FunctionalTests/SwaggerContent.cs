3bd0f2210a6291a79bf855014a20b092cb12931f
Francisco Sierra Luciarte <62906570+franciscofsl@users.noreply.github.com>
2025-04-30 18:23:07 +0200

    remove unused types

--- tests/Raftel.Api.FunctionalTests/SwaggerContent.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Text.Json.Serialization;
-
-namespace Raftel.Api.FunctionalTests;
-
-public class SwaggerContent
-{
-    [JsonPropertyName("schema")] public SwaggerSchema Schema { get; set; } = new();
-}
--- tests/Raftel.Api.FunctionalTests/SwaggerRequestBody.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Text.Json.Serialization;
-
-namespace Raftel.Api.FunctionalTests;
-
-public class SwaggerRequestBody
-{
-    [JsonPropertyName("content")] public Dictionary<string, SwaggerContent> Content { get; set; } = new();
-}
