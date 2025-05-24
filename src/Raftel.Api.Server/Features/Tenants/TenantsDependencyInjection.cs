7505702b25c411f20efe1af952b9f62ee4654cbf
Francisco Sierra Luciarte <62906570+franciscofsl@users.noreply.github.com>
2025-05-25 00:10:45 +0200

    feat: add GetAllTenants and GetCurrentTenant queries to Tenants API

--- src/Raftel.Api.Server/Features/Tenants/TenantsDependencyInjection.cs
+++ src/Raftel.Api.Server/Features/Tenants/TenantsDependencyInjection.cs
@@ -2,6 +2,8 @@ using Microsoft.AspNetCore.Routing;
 using Raftel.Api.Server.AutoEndpoints;
 using Raftel.Application.Features.Tenants.CreateTenant;
 using Raftel.Application.Features.Tenants.GetTenant;
+using Raftel.Application.Features.Tenants.GetAllTenants;
+using Raftel.Application.Features.Tenants.GetCurrentTenant;
 
 namespace Raftel.Api.Server.Features.Tenants;
 
@@ -15,6 +17,8 @@ public static class TenantsDependencyInjection
             group.BaseUri = "/api/tenants";
             group.AddCommand<CreateTenantCommand>("", HttpMethod.Post);
             group.AddQuery<GetTenantQuery, GetTenantResponse>("{id}", HttpMethod.Get);
+            group.AddQuery<GetAllTenantsQuery, List<GetAllTenantsResponse>>("", HttpMethod.Get);
+            group.AddQuery<GetCurrentTenantQuery, GetCurrentTenantResponse>("current", HttpMethod.Get);
         });
 
         return app;
