118e4b845c3ddfa20a1a082163bf6938a03170d6
Francisco Sierra Luciarte <62906570+franciscofsl@users.noreply.github.com>
2025-05-24 08:00:14 +0200

    feat: add Tenant and User tables with migration

---
+++ demo/Raftel.Demo.Infrastructure/Data/Migrations/20250524054749_Add_Tenants.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Raftel.Demo.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Add_Tenants : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Tenant",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Code = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tenant", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "User",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Surname = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_User", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tenant_Code",
+                table: "Tenant",
+                column: "Code",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_User_Email",
+                table: "User",
+                column: "Email");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Tenant");
+
+            migrationBuilder.DropTable(
+                name: "User");
+        }
+    }
+}
---
+++ tests/Raftel.Api.FunctionalTests/Features/MultitenancyTests.cs
@@ -0,0 +1,35 @@
+﻿using System.Net.Http.Json;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Raftel.Api.Client;
+using Raftel.Demo.Application.Pirates.GetPirateByFilter;
+using Shouldly;
+
+namespace Raftel.Api.FunctionalTests.Features;
+
+public class MultitenancyTests : IClassFixture<ApiTestFactory>
+{
+    private readonly HttpClient _client;
+
+    public MultitenancyTests(ApiTestFactory factory)
+    {
+        _client = factory.CreateClient(new WebApplicationFactoryClientOptions
+        {
+            BaseAddress = new Uri("https://localhost:5128")
+        });
+    }
+
+    [Fact]
+    public async Task ApiShould_CreateTenant()
+    {
+        await Should.NotThrowAsync(async () =>
+        {
+            var result = await _client.PostAsJsonAsync("/api/tenants", new
+            {
+                name = "Nombre del tenant",
+                code = "codigo",
+                description = "Descripción del tenant"
+            });
+            result.EnsureSuccessStatusCode();
+        });
+    }
+}
