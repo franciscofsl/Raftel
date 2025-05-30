81394ed25508e91b75ec4381c26e9d3c60b8ef81
Francisco Sierra Luciarte <62906570+franciscofsl@users.noreply.github.com>
2025-05-30 16:38:21 +0200

    feat(extensions) add WhereIf extension method for conditional querying

---
+++ src/Raftel.Shared/Extensions/QueryableExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq.Expressions;
+
+namespace Raftel.Shared.Extensions;
+
+public static class QueryableExtensions
+{
+    public static IQueryable<T> WhereIf<T>(this IQueryable<T> source, bool condition,
+        Expression<Func<T, bool>> predicate)
+        where T : class
+    {
+        return condition ? source.Where(predicate) : source;
+    }
+}
