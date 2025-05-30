b06cf0e7a0abdd2b0e741da1d193f1a090a752d8
Francisco Sierra Luciarte <62906570+franciscofsl@users.noreply.github.com>
2025-05-30 17:21:09 +0200

    feat(permissions) register AssignRoleToUser command and remove RolesRepository from DI

--- src/Raftel.Infrastructure/DependencyInjection.cs
+++ src/Raftel.Infrastructure/DependencyInjection.cs
@@ -8,14 +8,12 @@ using OpenIddict.Validation.AspNetCore;
 using Raftel.Application;
 using Raftel.Application.Abstractions.Authentication;
 using Raftel.Application.Abstractions.Multitenancy;
-using Raftel.Domain.Features.Authorization;
 using Raftel.Domain.Features.Tenants;
 using Raftel.Domain.Features.Users;
 using Raftel.Infrastructure.Authentication;
 using Raftel.Infrastructure.Data;
 using Raftel.Infrastructure.Data.Filters;
 using Raftel.Infrastructure.Data.Interceptors;
-using Raftel.Infrastructure.Data.Repositories.Authorization;
 using Raftel.Infrastructure.Data.Repositories.Tenants;
 using Raftel.Infrastructure.Data.Repositories.Users;
 using Raftel.Infrastructure.Multitenancy;
@@ -53,7 +51,6 @@ public static class DependencyInjection
         services.AddScoped<IAuthenticationService, AuthenticationService>();
         services.AddScoped(typeof(IUsersRepository), typeof(UsersRepository<TDbContext>));
         services.AddScoped(typeof(ITenantsRepository), typeof(TenantsRepository<TDbContext>));
-        services.AddScoped(typeof(IRolesRepository), typeof(RolesRepository<TDbContext>));
 
         services.AddOpenIddict()
             .AddCore(opt => opt.UseEntityFrameworkCore().UseDbContext<TDbContext>())
